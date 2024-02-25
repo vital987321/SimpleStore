2b4b7cf6f9376bbab6da894e7d3c585b2f0facee
vital987321 <vitaliyvelichko@gmail.com>
2024-02-26 00:24:12 +0100

    Register

--- simplestore/urls.py
+++ simplestore/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from storeapp.views import Register
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
+    path('register/', Register.as_view(), name='register')
 ]
--- storeapp/views.py
+++ storeapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import UserCreationForm
 
-# Create your views here.
+class Register(CreateView):
+    form_class=UserCreationForm
+    template_name='register.html'
+    success_url='/'
