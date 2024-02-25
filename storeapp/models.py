f21a0fbacdae010cf84b7d920ef4ee1425bc516a
vital987321 <vitaliyvelichko@gmail.com>
2024-02-25 13:32:14 +0100

    models are added

--- storeapp/models.py
+++ storeapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    name=models.CharField(max_length=30)
+    wallet=models.DecimalField(decimal_places=2, max_digits=12)
+    
+
+class Product(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField(null=True, blank=True)
+    price=models.DecimalField(decimal_places=2, max_digits=12)
+    stock_amount=models.IntegerField(default=0)
+    # picture=models.ImageField(blank=True, null=True)
+
+class Purchase(models.Model):
+    client=models.ForeignKey(User, on_delete=models.CASCADE, related_name='client')
+    product_amount=models.PositiveIntegerField()
+    purchasing_time=models.DateField(auto_now_add=True)
+
+class Refund(models.Model):
+    purchase=models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    
