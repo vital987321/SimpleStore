8f9baf6b8746812cb4bdde20001da60850aef589
Vitalii Velychko <vitaliivelychko1@gmail.com>
2024-02-28 15:46:54 +0100

    before changing purchaseView

--- storeapp/views.py
+++ storeapp/views.py
@@ -19,29 +19,60 @@ class Login(LoginView):
     def get_success_url(self):
         return self.success_url
     
+class Logout(LoginRequiredMixin, LogoutView):
+    next_page = '/'
+    login_url = 'login/'
+
+
+# class IndexView(LoginRequiredMixin, ListView):
+#     template_name='index.html'
+#     login_url = 'login/'
+#     model=Product
+#     # paginate_by=10
+    
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['form'] = PurchaseForm()
+#         return context
+
+
+# class PurchaseView(CreateView):
+#     form_class=PurchaseForm
+#     template_name='purchase.html'
+#     success_url=reverse_lazy('purchase')
+
+#     def form_valid(self, form):
+#         obj = form.save(commit=False)
+#         obj.client = self.request.user
+#         obj.product = Product.objects.get(pk=form.data['productID'])
+#         obj.save()
+#         return super().form_valid(form=form)
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         # print('\n get_context_data:')
+#         # print(context['form'].data)
+#         return context
+
 
 class IndexView(LoginRequiredMixin, ListView):
     template_name='index.html'
     login_url = 'login/'
     model=Product
     # paginate_by=10
-    
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = PurchaseForm()
         return context
 
 
-class Logout(LoginRequiredMixin, LogoutView):
-    next_page = '/'
-    login_url = 'login/'
-
-
 class PurchaseView(CreateView):
-    form_class=PurchaseForm
+    
     template_name='purchase.html'
     success_url=reverse_lazy('purchase')
-
+    form_class=PurchaseForm
     def form_valid(self, form):
         obj = form.save(commit=False)
         obj.client = self.request.user
@@ -54,7 +85,6 @@ class PurchaseView(CreateView):
         # print('\n get_context_data:')
         # print(context['form'].data)
         return context
-    
 
 class MyOrdersView(LoginRequiredMixin, ListView):
     template_name = 'myorders.html'
